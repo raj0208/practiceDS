662be79fb29f420ba1550f980e94dc7cf6900468
raj0208 <raj280@gmail.com>
2021-09-13 14:11:36 +0800

    Added Java DS and Algo practice project

---
+++ src/com/raj/practice/LeetCode.java
@@ -0,0 +1,11 @@
+package com.raj.practice;
+
+public class LeetCode {
+    public static void main(String[] args) {
+        twoSum();
+    }
+
+    private static void twoSum() {
+        System.out.println("Hello World");
+    }
+}
