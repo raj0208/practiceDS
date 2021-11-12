7bf73e1f8b9dde1cb2c070926e31e7d6dfea4045
raj0208 <raj280@gmail.com>
2021-11-12 09:02:47 +0800

    crop

--- src/com/raj/practice/Misc/CropText.java
+++ src/com/raj/practice/Misc/CropText.java
@@ -2,13 +2,55 @@ package com.raj.practice.Misc;
 
 public class CropText {
     public static void main(String[] args) {
-        String text = "very-long-input-will-be-truncated";
-        System.out.println(cropText(text, 20));
-        text = "a-b-c-d-e-f-g-veryveryverylongword";
-        System.out.println(cropText(text, 20));
+        String text = "Codility We test coders";
+        int size= 14;
+        text = "The quick brown fox jumps over the lazy dog";
+        size = 39;
+//        String text = "very-long-input-will-be-truncated";
+        System.out.println(cropText(text, 39));
+//        text = "a-b-c-d-e-f-g-veryveryverylongword";
+//        System.out.println(cropText(text, 20));
     }
 
-    private static String cropText(String text, int length) {
+    public String solution(String message, int K) {
+        // write your code in Java SE 8
+        if (K >= message.length()) {
+            return message;
+        }
+
+        String[] words = message.split(" ");
+        String result = "";
+
+        for (int i = 0; i < words.length; i++) {
+            String tmp = result +  " " + words[i];
+            if (tmp.trim().length() <= K) {
+                result = tmp.trim();
+            } else {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+
+    private static String cropText(String message, int K) {
+        String[] words = message.split(" ");
+        String result = "";
+
+        for (int i = 0; i < words.length; i++) {
+            String tmp = result +  " " + words[i];
+            if (tmp.trim().length() <= K) {
+                result = tmp.trim();
+            } else {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    private static String cropText1(String text, int length) {
         // validate parameters
         if (text == null || length == 0) {
             throw new IllegalArgumentException();
@@ -18,18 +60,21 @@ public class CropText {
             return text;
         }
 
-        String SEPARATOR = "-";
-
-        int maxLength = text.indexOf(SEPARATOR, length);
+        String SEPARATOR = " ";
+        int maxLength = length;
         String result = null;
-        if (maxLength >= length) {
-            result = text.substring(0, maxLength);
-        } else {
-            maxLength = text.lastIndexOf(SEPARATOR);
-            if (maxLength != -1) {
+
+        while(result == null) {
+            maxLength = text.indexOf(SEPARATOR, maxLength);
+            if (maxLength <= length) {
                 result = text.substring(0, maxLength);
             } else {
-                result = text;
+                maxLength = text.indexOf(SEPARATOR, maxLength);; //text.lastIndexOf(SEPARATOR);
+//                if (maxLength != -1) {
+//                    result = text.substring(0, maxLength);
+//                } else {
+//                    result = text;
+//                }
             }
         }
 
