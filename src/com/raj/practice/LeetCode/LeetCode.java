a3e8bf3849244096668420301559c38532ac32ad
raj0208 <raj280@gmail.com>
2021-11-18 14:40:25 +0800

    more leetcode problems

--- src/com/raj/practice/LeetCode/LeetCode.java
+++ src/com/raj/practice/LeetCode/LeetCode.java
@@ -1,16 +1,15 @@
 package com.raj.practice.LeetCode;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
+import java.util.*;
 
 public class LeetCode {
 
     public static void main(String[] args) {
 //        longestPalindromeSubString();
 //        lengthOfLongestSubstring();
-        decodeString();
+//        decodeString();
+//        wordBreak();
+        sortColors();
     }
 
     public static void longestPalindromeSubString() {
@@ -89,5 +88,54 @@ public class LeetCode {
         }
 
         System.out.println(sb.toString());
+    }
+
+    public static void wordBreak() {
+        String s = "leetcode";
+        HashSet<String> dict = new HashSet<>();
+        dict.add("leet");
+        dict.add("code");
+
+        boolean[] dp = new boolean[s.length()+1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i ; j++) {
+                if (dp[j] && dict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(dp[s.length()]);
+    }
+
+    public static void sortColors() {
+        int[] arr = new int[] {2,0,2,1,1,0};
+
+        int low = 0, high = arr.length - 1, curr = 0;
+
+        while(curr <= high) {
+            switch(arr[curr]) {
+                case 0:
+                    arr[curr] ^= arr[low];
+                    arr[low] ^= arr[curr];
+                    arr[curr] ^= arr[low];;
+                    curr++;
+                    low++;
+                    break;
+                case 1:
+                    curr++;
+                    break;
+                case 2:
+                    arr[curr] ^= arr[high];
+                    arr[high] ^= arr[curr];
+                    arr[curr] ^= arr[high];;
+                    high--;
+                    break;
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
     }
 }
